14b47567bac9f1127f3fe625a785ad92bb0df038
zenpk <99558412+zenpk@users.noreply.github.com>
2023-10-30 00:40:47 +0800

    rm plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      injectRegister: "auto",
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: [
       {
